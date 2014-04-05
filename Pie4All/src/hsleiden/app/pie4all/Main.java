8ad710a817d0ecbf57cf87aa483d85cc4041956f
peter-de-ridder <peter-de-ridder@live.nl>
2014-04-05 16:36:08 +0200

    Basis Xml opgezet
    Basis van xml opgezet voor hoofdpagina,bestelpagina, productpagina

---
+++ Pie4All/src/hsleiden/app/pie4all/Main.java
@@ -0,0 +1,21 @@
+package hsleiden.app.pie4all;
+
+import hsleiden.app.pie4all.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class Main extends Activity {
+	
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+	
+}
